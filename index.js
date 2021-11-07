fb19a8b072e8529b9d90bd3fb322da31ca4911df
Soopyboo32 <49228220+Soopyboo32@users.noreply.github.com>
2021-11-07 15:42:22 +0800

    add guimanager polyfill

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 /// <reference types="../CTAutocomplete" />
 /// <reference lib="es2015" />
 
+import SoopyGuiElement from "../guimanager/GuiElement/SoopyGuiElement.js"
+import SoopyBoxElement from "../guimanager/GuiElement/SoopyBoxElement"
+
+SoopyGuiElement.prototype.clearChildren = function (){
+    let theParent = this.innerObjectPaddingThing || this
+    theParent.children.forEach(child => {
+        child.setParent(undefined)
+    });
+    theParent.children = []
+    return this
+}
+SoopyBoxElement.prototype.clearChildren = function (){
+    let theParent = this.innerObjectPaddingThing || this
+    theParent.children.forEach(child => {
+        child.setParent(undefined)
+    });
+    theParent.children = []
+    return this
+}
 class SoopyAddons {
     constructor(){
         this.FeatureManager = require("./featureClass/featureManager.js")
