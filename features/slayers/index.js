b8b310b12ff8e4e932063fffafdda410e3c19df7
EmeraldMerchant <96396730+EmeraldMerchant@users.noreply.github.com>
2022-06-23 17:41:00 +0800

    removed logging
    ^

--- features/slayers/index.js
+++ features/slayers/index.js
@@ -385,8 +385,6 @@ class Slayers extends Feature {
 				if (!this.bossSpawnedMessage && e instanceof net.minecraft.entity.item.EntityArmorStand) {
 					let mobName = `${e[m.getCustomNameTag]().removeFormatting().split(" ")[0]} ${e[m.getCustomNameTag]().removeFormatting().split(" ")[1]}`
 					if (this.Miniboss[this.lastSlayerType].has(mobName)) {
-						ChatLib.chat("a")
-						ChatLib.chat(!this.minibossEntity.map(a=>a[0].getUUID().toString()).includes(e[m.getEntityId.Entity]().toString()))
 						if (this.BoxAroundMiniboss.getValue() && !this.minibossEntity.map(a=>a[0].getUUID().toString()).includes(e[m.getEntityId.Entity]().toString())) {
 							this.minibossEntity.push([new Entity(e), this.lastSlayerType]);
 						}
